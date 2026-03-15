4b22dc2e6cbf5b0827ba25e3f8c8365055a63768
upendra-011 <upendrasagar05@gmail.com>
2026-03-15 19:55:49 +0530

    Add files via upload

---
+++ login_page.html.py
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+
+<h2>Login Page</h2>
+
+<form>
+    <label>Username:</label><br>
+    <input type="text" name="username"><br><br>
+
+    <label>Password:</label><br>
+    <input type="password" name="password"><br><br>
+
+    <input type="submit" value="Login">
+</form>
+
+<p>Don't have an account? <a href="#">Sign up</a></p>
+
+</body>
+</html>
